bff161e2b4d268731400db818fe4aa5c85e7db22
Hubert Zhang <hzhang@pivotal.io>
2019-10-22 09:15:31 +0000

    Pin buffers in batched way.

---
+++ contrib/vectorize_engine/execTuples.h
@@ -0,0 +1,27 @@
+#ifndef VECTOR_ENGINE_EXECUTOR_H
+#define VECTOR_ENGINE_EXECUTOR_H
+
+#include "postgres.h"
+
+#include "executor/execdesc.h"
+#include "nodes/parsenodes.h"
+#include "executor/tuptable.h"
+#include "storage/buf.h"
+#include "vtype.h"
+
+typedef struct VectorTupleSlot
+{
+	TupleTableSlot	tts;
+	int32			dim;
+	Buffer			tts_buffers[BATCHSIZE];	
+} VectorTupleSlot;
+
+extern TupleTableSlot *VExecClearTuple(TupleTableSlot *slot);
+extern void VExecPinSlotBuffers(TupleTableSlot *slot, Buffer buffer, int idx);
+/*
+ * prototypes from functions in execTuples.c
+ */
+extern void VExecInitResultTupleSlot(EState *estate, PlanState *planstate);
+extern void VExecInitScanTupleSlot(EState *estate, ScanState *scanstate);
+
+#endif
